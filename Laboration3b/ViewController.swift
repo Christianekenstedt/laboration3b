8c89713ed49015cdab9614b5f0d2ae7f622a55af
Christian <asd>
2016-12-05 10:53:59 +0100

    Initial Commit

---
+++ Laboration3b/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Laboration3b
+//
+//  Created by Christian Ekenstedt on 2016-12-05.
+//  Copyright © 2016 Christian Ekenstedt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
